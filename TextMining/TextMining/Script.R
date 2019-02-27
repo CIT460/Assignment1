441b3cb997cf9469efa8a6cd5f7356b379ebd737
Bailey Miller <bjm19@pct.edu>
2019-02-27 15:52:45 -0500

    Stuff

--- TextMining/TextMining/Script.R
+++ TextMining/TextMining/Script.R
@@ -1,14 +1,24 @@
-getwd()
 install.packages("tm")
 library(tm)
 
 docs <- Corpus(DirSource("textmining/"))
 
-? Corpus
-class(docs)
 length(docs)
 
-docs[[1]]
-writeLines(as.character(docs[[30]]))
+
+# Create a function
+toSpace <- content_transformer((function(x, pattern) return(gsub(pattern, "", x))))
+writelines(as.character(docs[[1]]))
+docs <- tm_map(docs, toSpace, "-")
+docs <- tm_map(docs, removePunctuation)
+docs <- tm_map(docs, toLower)
+docs <- tm_map(docs, removeNumbers)
+
+
+
+
+
+
+
 
 
